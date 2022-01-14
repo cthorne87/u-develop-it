87b7f7400348756edf61f31d9bf34b3672cb8622
Chris Thorne <u1400900@umail.utah.edu>
2022-01-13 18:55:07 -0700

    express

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
