c92d920752e270bef05742533e3d25499635bdc1
Chris Thorne <u1400900@umail.utah.edu>
2022-02-02 12:15:37 -0700

    12.2.4

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+const mysql = require('mysql2');
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+require('dotenv').config()
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'election'
+  },
+  console.log('connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+// Default response for any other request (Not Found) catchall
 app.use((req, res) => {
   res.status(404).end();
 });
